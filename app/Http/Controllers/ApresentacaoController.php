4a4448933f80459f145fc19d54003e0cb1572b15
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2019-03-10 13:43:00 -0300

    initi project

---
+++ app/Http/Controllers/ApresentacaoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApresentacaoController extends Controller
+{
+    public function index () {
+
+        return view('apresentacao.index');
+    }
+}
